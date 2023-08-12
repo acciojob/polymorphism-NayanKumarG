a9f33288d2cc835d34fe55cd92c2fc27f41f30f9
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-08-12 11:33:14 +0530

    Attempted polymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x,int y)
+        {
+            return x;
+        }
+        public int product(int x,int y,int z)
+        {
+            return x;
+        }
+        public double product(double x,double y)
+        {
+            return x;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,6);
+        p.product(5,6,7);
+        p.product(5.01,3.00);
+    }
 }
